a4026981b13236d38a929f6f062fa104c43f107e
Haykal <moragasfarage.haykal@gmail.com>
2025-08-12 16:10:17 -0300

    zap3

--- SGJ-2025/Assets/Scripts/InvetorySystem/Inventory.cs
+++ SGJ-2025/Assets/Scripts/InvetorySystem/Inventory.cs
@@ -4,53 +4,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-[RequireComponent(typeof(Collider2D))]
+// [RequireComponent(typeof(Collider2D))]
 public class Inventory : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] InventoryUI ui;
+    // [Header("References")]
+    // [SerializeField] InventoryUI ui;
     // [SerializeField] AudioSource audioSource; -> PARTE DE AUDIO VER COM IQUE
 
-    [Header("Prefabs")]
-    [SerializeField] GameObject droppedItemPrefab;
+    // [Header("Prefabs")]
+    // [SerializeField] GameObject droppedItemPrefab;
 
     // [Header("Audio Clips")] -> PARTE DE AUDIO VER COM IQUE
     // [SerializeField] AudioClip pickUpItemAudio;
     // [SerializeField] AudioClip droppItemAudio;
 
-    [Header("State")]
-    [SerializeField] SerializedDictionary<string, InventoryAssets> inventory = new();
+    // [Header("State")]
+    // [SerializeField] SerializedDictionary<string, InventoryAssets> inventory = new();
 
-    public void OTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("DroppedItem") && Input.GetMouseButtonDown(0))
+        if (collision.CompareTag("DroppedItem"))
         {
-            var droppedItem = collision.GetComponent<DroppedItem>();
-            if (droppedItem.pickedUp)
-            {
-                return;
-            }
-            droppedItem.pickedUp = true;
-            AddItem(droppedItem.item);
-            Destroy(collision.gameObject);
+            Debug.Log("zap");
+
+            // var droppedItem = collision.GetComponent<DroppedItem>();
+            // if (droppedItem.pickedUp)
+            // {
+            //     return;
+            // }
+            // droppedItem.pickedUp = true;
+            // AddItem(droppedItem.item);
+            // Destroy(collision.gameObject);
             // AudioSource.PlayOneShot(pickUpItemAudio);
         }
     }
 
-    void AddItem(InventoryAssets item)
-    {
-        var inventoryId = Guid.NewGuid().ToString();
-        inventory.Add(inventoryId, item);
-        // ui.AddUIItem(inventoryId, item);
-    }
+    // void AddItem(InventoryAssets item)
+    // {
+    //     var inventoryId = Guid.NewGuid().ToString();
+    //     inventory.Add(inventoryId, item);
+    //     // ui.AddUIItem(inventoryId, item);
+    // }
 
-    public void DropItem(string inventoryId)
-    {
-        var droppedItem = Instantiate(droppedItemPrefab, transform.position, Quaternion.identity).GetComponent<DroppedItem>();
-        var item = inventory.GetValueOrDefault(inventoryId);
-        droppedItem.Initialize(item);
-        inventory.Remove(inventoryId);
-        // ui.RemoveUIItem(inventoryId);
-        // AudioSource.PlayOneShot(droppItemAudio);
-    }
+    // public void DropItem(string inventoryId)
+    // {
+    //     var droppedItem = Instantiate(droppedItemPrefab, transform.position, Quaternion.identity).GetComponent<DroppedItem>();
+    //     var item = inventory.GetValueOrDefault(inventoryId);
+    //     droppedItem.Initialize(item);
+    //     inventory.Remove(inventoryId);
+    //     // ui.RemoveUIItem(inventoryId);
+    //     // AudioSource.PlayOneShot(droppItemAudio);
+    // }
 }

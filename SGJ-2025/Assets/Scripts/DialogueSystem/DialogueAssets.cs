1deb3eef12a3c4e73b82ca43939b6f289a0d5128
Haykal <moragasfarage.haykal@gmail.com>
2025-08-12 13:56:40 -0300

    zap

--- SGJ-2025/Assets/Scripts/DialogueSystem/DialogueAssets.cs
+++ SGJ-2025/Assets/Scripts/DialogueSystem/DialogueAssets.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "ScriptableOject", menuName = "Dialogue/New Dialogue")]
 public class DialogueAssets : ScriptableObject
 {
     [TextArea]
--- SGJ-2025/Assets/Scripts/InvetorySystem/InventoryAssets.cs
+++ SGJ-2025/Assets/Scripts/InvetorySystem/InventoryAssets.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "ScriptableOject", menuName = "Inventory/New Item")]
 public class InventoryAssets : ScriptableObject
 {
     public string itemName;

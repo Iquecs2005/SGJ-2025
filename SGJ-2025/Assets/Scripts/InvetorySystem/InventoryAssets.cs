04491b6e654be1292b596b4169562d6d68e348db
Haykal <moragasfarage.haykal@gmail.com>
2025-08-12 15:54:44 -0300

    zap2

--- SGJ-2025/Assets/Scripts/InvetorySystem/InventoryAssets.cs
+++ SGJ-2025/Assets/Scripts/InvetorySystem/InventoryAssets.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ScriptableOject", menuName = "Inventory/New Item")]
+[CreateAssetMenu(fileName = "ScriptableOject", menuName = "Inventory/New Item", order = 1)]
 public class InventoryAssets : ScriptableObject
 {
-    public string itemName;
-    [TextArea]
-    public string description;
+    public string id;
     public Sprite icon;
+    public GameObject prefab;
+    [TextArea] public string description;
 
-    public InventoryAssets(string name, string description, Sprite icon)
-    {
-        itemName = name;
-        this.description = description;
-        this.icon = icon;
-    }
 }

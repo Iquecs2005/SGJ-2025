105835960764895b94cd8ff45f89731c195e26a6
Haykal <ferramentas@haykal.com.br>
2025-08-26 21:26:42 -0300

    Slider de Settings salva valor

--- SGJ-2025/Assets/Scripts/Singletons/AudioManager/AudioManager.cs
+++ SGJ-2025/Assets/Scripts/Singletons/AudioManager/AudioManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager instance { get; private set; } 
-    public static SFXManager sfxManager { get; private set; } 
-    public static MusicManager musicManager { get; private set; } 
+    public static AudioManager instance { get; private set; }
+    public static SFXManager sfxManager { get; private set; }
+    public static MusicManager musicManager { get; private set; }
+
+    [SerializeField] AudioMixer mixer;
+
+    [HideInInspector] public const string MASTER_KEY = "masterVolume";
+    [HideInInspector] public const string MUSIC_KEY = "musicVolume";
+    [HideInInspector] public const string SFX_KEY = "sfxVolume";
 
     private void Awake()
     {
-        if (instance != null && instance != this) 
+        if (instance != null && instance != this)
         {
             sfxManager = GetComponentInChildren<SFXManager>();
             Destroy(sfxManager.gameObject);
@@ -18,7 +25,7 @@ public class AudioManager : MonoBehaviour
             Destroy(musicManager.gameObject);
             Destroy(this);
         }
-        else 
+        else
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
@@ -28,5 +35,18 @@ public class AudioManager : MonoBehaviour
             musicManager = GetComponentInChildren<MusicManager>();
             musicManager.Initialization();
         }
+
+        LoadVolume();
+    }
+
+    void LoadVolume()  //Volume salvo no audioSlider
+    {
+        float masterVolume = PlayerPrefs.GetFloat(MASTER_KEY, 1f);
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+
+        mixer.SetFloat(AudioSlider.MIXER_MASTER, Mathf.Log10(masterVolume) * 20);
+        mixer.SetFloat(AudioSlider.MIXER_MUSIC, Mathf.Log10(musicVolume) * 20);
+        mixer.SetFloat(AudioSlider.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
     }
 }
